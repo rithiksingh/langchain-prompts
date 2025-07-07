809799ec09d45c686cc0e66ec7de9aca71aa27ae
Hrithik Singh <rithiksingh666@gmail.com>
2025-07-07 10:58:27 +0300

    chatbot using messages

--- chatbot.py
+++ chatbot.py
@@ -6,10 +6,19 @@ load_dotenv()
 
 model=ChatOpenAI()
 
-messages= [
+chat_history= [
     SystemMessage(content="You are a helpful assistant.")
 ]
 
 while True:
-    
+    user_input= input("You: ")
+    if user_input == "exit":
+        break
+    chat_history.append(HumanMessage(content= user_input))
+    response=model.invoke(chat_history)
+    chat_history.append(AIMessage(content=response.content))
+    print(response.content)
+
+print(chat_history)
+
 
